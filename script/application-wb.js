9d836a83c049371a44bd4ab8dd5418edbb9df713
Jean-Baptiste de Borman <jb@noloading.com>
2014-01-17 12:24:22 +0100

    Revert "adding ie8 detection"
    This reverts commit 2df2ce3cede61d6d5e0f8e268e68ab08a939730a.

--- script/application-wb.js
+++ script/application-wb.js
@@ -2,159 +2,156 @@
  * @author jb@noloading.com
  */
 /*jslint vars:true, white:true, nomen:true, plusplus:true */
-/*global $,SimpleMenu,UserAgent,Modernizr*/
+/*global $,SimpleMenu,Modernizr*/
 
-$(window).load(function() {"use strict";
 
-	//$('html').addClass('lg-fr');
+$(window).load(function()  {"use strict";
 
-	if (UserAgent.msie () > 0 && UserAgent.msie() < 9) {
-		$('html').addClass('ie8');
+    //$('html').addClass('lg-fr');
 
-	}
+    $('#application').css('display', 'block');
+    $('.loading').detach (); 
+    $('#illustration').addClass ("animStage"); 
 
-	$('#application').css('display', 'block');
-	$('.loading').detach();
-	$('#illustration').addClass("animStage");
+    
 
-	var userLang_str = navigator.language || navigator.userLanguage, POPUP_HEIGHT = 300, buttons_$ = $('.button'), lastPopup_$, popups_$, buttons_array = [], popups_array = [];
+    var userLang_str = navigator.language || navigator.userLanguage, POPUP_HEIGHT = 300, buttons_$ = $('.button'), lastPopup_$, popups_$, buttons_array = [], popups_array = [];
 
-	//data-buttonPos
-	var languageMenu_$ = $('#languageMenu');
-	var languageMenu = new SimpleMenu(languageMenu_$);
-	var selectedLanguage_str;
+    //data-buttonPos
+    var languageMenu_$ = $('#languageMenu');
+    var languageMenu = new SimpleMenu(languageMenu_$);
+    var selectedLanguage_str;
 
-	if (userLang_str.toString().toLowerCase().indexOf('nl') !== -1) {
-		selectedLanguage_str = 'nl';
+    if (userLang_str.toString().toLowerCase().indexOf('nl') !== -1) {
+        selectedLanguage_str = 'nl';
 
-	} else {
-		selectedLanguage_str = 'fr';
+    } else {
+        selectedLanguage_str = 'fr';
 
-	}
+    }
 
-	var selection_str = "data-id = '" + selectedLanguage_str + "'";
+    var selection_str = "data-id = '" + selectedLanguage_str + "'";
 
-	languageMenu_$.find("span[" + selection_str + "]").trigger('click');
-	$('html').toggleClass("lg-" + selectedLanguage_str);
+    languageMenu_$.find("span[" + selection_str + "]").trigger('click');
+    $('html').toggleClass("lg-" + selectedLanguage_str);
 
-	languageMenu.onSelect = function() {
-		//alert ("onselect");
-		var lg_str = this.selected_$.attr('data-id');
-		$('html').toggleClass("lg-fr", false);
-		$('html').toggleClass("lg-nl", false);
-		$('html').toggleClass("lg-" + lg_str, true);
+    languageMenu.onSelect = function() {
+        //alert ("onselect");
+        var lg_str = this.selected_$.attr('data-id');
+        $('html').toggleClass("lg-fr", false);
+        $('html').toggleClass("lg-nl", false);
+        $('html').toggleClass("lg-" + lg_str, true);
 
-		//alert (lg_str);
+        //alert (lg_str);
 
-	};
+    };
 
-	function placeButtons() {
+    function placeButtons() {
 
-		popups_$ = $('.popup').detach();
-		$('#application').append($('header'));
+        popups_$ = $('.popup').detach();
+        $('#application').append($('header'));
 
-		popups_$.each(function(index, element) {
+        popups_$.each(function(index, element) {
 
-			var close_$ = $('<div></div>'), button_$ = $('<div></div>'), element_$ = $(element), pos_array = element_$.attr('data-buttonPos').split(',');
-			close_$.addClass('closeButton');
-			close_$.bind('click', removeLast);
-			element_$.append(close_$);
-			button_$.addClass('button');
-			button_$.css('top', Number(pos_array[1] - 5));
-			button_$.css('left', Number(pos_array[0] - 5));
-			button_$.attr('data-index', index);
-			button_$.bind('mouseover', clickButton);
-			element_$.attr('data-index', index);
+            var close_$ = $('<div></div>'), button_$ = $('<div></div>'), element_$ = $(element), pos_array = element_$.attr('data-buttonPos').split(',');
+            close_$.addClass('closeButton');
+            close_$.bind('click', removeLast);
+            element_$.append(close_$);
+            button_$.addClass('button');
+            button_$.css('top', Number(pos_array[1] - 5));
+            button_$.css('left', Number(pos_array[0] - 5));
+            button_$.attr('data-index', index);
+            button_$.bind('mouseover', clickButton);
+            element_$.attr('data-index', index);
 
-			var delay_num = Number(pos_array[1] * 8) + 500;
-			setTimeout(function() {
-				//alert ("timeOut");
+            var delay_num = Number(pos_array[1] * 8) + 500;
+            setTimeout(function() {
+                //alert ("timeOut");
 
-				$('#application').append(button_$);
+                $('#application').append(button_$);
 
-			}, delay_num);
-			setTimeout(function() {
-				button_$.addClass('glow');
+            }, delay_num);
+            setTimeout(function() {
+                button_$.addClass('glow');
 
-			}, delay_num + 3000);
+            }, delay_num + 3000);
 
-			popups_array.push(element_$);
-			buttons_array.push(button_$);
+            popups_array.push(element_$);
+            buttons_array.push(button_$);
 
-		});
-	}
+        });
+    }
 
-	function clickButton() {
+    function clickButton() {
 
-		//alert('click');
+        //alert('click');
 
-		var popup_$, this_$ = $(this), top_num, left_num, height_num, index;
+        var popup_$, this_$ = $(this), top_num, left_num, height_num, index;
 
-		index = this_$.attr('data-index');
+        index = this_$.attr('data-index');
 
-		popup_$ = $(popups_$[index]);
-		popup_$.stop();
-		popup_$.css('opacity', 1);
-		popup_$.css('display', 'block');
+        popup_$ = $(popups_$[index]);
+        popup_$.stop();
+        popup_$.css('opacity', 1);
+        popup_$.css('display', 'block');
 
-		if (lastPopup_$ !== undefined) {
-			//console.log (lastPopup_$.attr('data-index') +" / " + popup_$.attr('data-index'));
+        if (lastPopup_$ !== undefined) {
+            //console.log (lastPopup_$.attr('data-index') +" / " + popup_$.attr('data-index'));
 
-			if (lastPopup_$.attr('data-index') !== popup_$.attr('data-index')) {
+            if (lastPopup_$.attr('data-index') !== popup_$.attr('data-index')) {
 
-				removeLast();
-			}
-		}
+                removeLast();
+            }
+        }
 
-		this_$.addClass('selected');
-		lastPopup_$ = popup_$;
-		popup_$.addClass('reveal');
+        this_$.addClass('selected');
+        lastPopup_$ = popup_$;
+        popup_$.addClass('reveal');
 
-		$('#application').append(popup_$);
+        $('#application').append(popup_$);
 
-		if (!Modernizr.touch) {
-			left_num = this_$.position().left + this_$.width() / 2 - popup_$.width() / 2;
-			if (left_num < 10) {
-				left_num = 10;
-			}
-			var max_pos = $('#application').width() - popup_$.width() - 10;
-			if (left_num > max_pos) {
-				left_num = max_pos;
-			}
-		}
+        if (!Modernizr.touch) {
+            left_num = this_$.position().left + this_$.width() / 2 - popup_$.width() / 2;
+            if (left_num < 10) {
+                left_num = 10;
+            }
+            var max_pos = $('#application').width() - popup_$.width() - 10;
+            if (left_num > max_pos) {
+                left_num = max_pos;
+            }
+        }
 
-		height_num = 'auto';
+        height_num = 'auto';
 
-		popup_$.css({
+        popup_$.css({
 
-			left : left_num,
-			height : height_num
-		});
-		lastPopup_$ = popup_$;
+            left : left_num,
+            height : height_num
+        });
+        lastPopup_$ = popup_$;
 
-	}
+    }
 
-	function removeLast() {
-		if (lastPopup_$ !== undefined) {
-			var index, button_$;
+    function removeLast() {
+        if (lastPopup_$ !== undefined) {
+            var index, button_$;
 
-			index = lastPopup_$.attr('data-index');
+            index = lastPopup_$.attr('data-index');
 
-			button_$ = buttons_array[index];
-			button_$.removeClass('selected');
-			lastPopup_$.fadeOut(300, function() {
+            button_$ = buttons_array[index];
+            button_$.removeClass('selected');
+            lastPopup_$.fadeOut(300, function() {
 
-				$(this).detach();
+                $(this).detach();
 
-			});
+            });
 
-		}
-	}
+        }
+    }
+    placeButtons();
 
-	placeButtons();
+    //alert($('buttons').length);
 
-	//alert($('buttons').length);
-
-	//buttons_$.bind('click', clickButton);
+    //buttons_$.bind('click', clickButton);
 
 });
